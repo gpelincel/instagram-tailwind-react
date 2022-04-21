61a86f3c173009fade282494fc93da8992770e26
Gabriel Pelincel Rmalho <gabrielpelincel@gmail.com>
2022-04-21 19:11:11 -0300

    Starting react

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import { Navbar } from './components/navbar';
 import { Suggestion } from './components/followSuggestion';
+import { StoriesBar } from './components/stories';
+import { Story } from './components/story';
 
 function App() {
   return (
     <body className="bg-gray-100">
-      <Navbar />
-      <Suggestion />
+      <StoriesBar>
+        <Story />
+      </StoriesBar>
     </body>
   );
 }
